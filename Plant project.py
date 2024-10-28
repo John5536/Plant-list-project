5ae094dc88e2cb85f26608a3ccf22510c4ffd5f9
John5536 <rrivers52@gmail.com>
2024-10-27 21:21:52 -0400

    first push

--- Plant project.py
+++ Plant project.py
@@ -1,3 +1,12 @@
 import random
 
-print(What kind of plant do you own?)
+
+num_plants = 0
+current_plants=[]
+plants_inputed = input("what kind of plant do you own?")
+if plants_inputed not in current_plants:
+    current_plants.append(plants_inputed)
+    num_plants += 1
+print(f"you have just added {plants_inputed}")
+print(current_plants, num_plants)
+print(f"Your current plants{current_plants}")
