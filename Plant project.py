04264cc759df9f51d2cc5e325c70e0b99194d0da
John5536 <rrivers52@gmail.com>
2024-10-31 08:26:27 -0400

    fixed saved watered plants list error. Rest of report is in the read me

--- Plant project.py
+++ Plant project.py
@@ -6,14 +6,24 @@ num_plants = 0
 current_plants=[]
 plant_watered = []
 filename = "current_plants.pkl"
+saved_water_list ="plant_watered.pkl"
+#loads current saved list data
+try:
+    with open(filename, 'rb') as file:
+        current_plants =pickle.load(file)
+except FileNotFoundError:
+    current_plants = []
+
+try:
+    with open(saved_water_list, 'rb') as file:
+        plant_watered =pickle.load(file)
+except FileNotFoundError:
+        plant_watered = []   
+
+#adds plants to the current_plant list.
+
 def adding_plants_to_list():
     global current_plants, num_plants
-
-    try:
-        with open(filename, 'rb') as file:
-            current_plants =pickle.load(file)
-    except FileNotFoundError:
-        current_plants = []
 
     plants_inputed = input("Would you like to add a plant to your list? If so type its name. Type no for other options \n").lower()
     if plants_inputed == "no":
@@ -40,49 +50,45 @@ def adding_plants_to_list():
         print(current_plants, len(current_plants))
     else:
         print("ok")
- 
+#adds plants to plant_watered list.
 def watering_plants_func():
     #plant on list watered. Goal/ add date to plant watered. Make it saved to the correct plant. 
     global plant_watered
 
-    try:
-        with open(filename, 'rb') as file:
-            plant_watered =pickle.load(file)
-    except FileNotFoundError:
-        plant_watered = []
+    
 
     which_plant_watered = input("which plant did you water?").lower()
     plant_watered.append(which_plant_watered)
-    print(f"{which_plant_watered}was watered and added to your list. You can type clear to clear your list")
+    print(f"{which_plant_watered}was watered and added to your list.")
     print(plant_watered)
-    YN_another_plant = input("any other plants watered? type yes or no").lower()
-    if YN_another_plant == "yes":
+    another_plant_yes = input("any other plants watered? type yes or no. You can type clear to clear your list").lower()
+    if another_plant_yes == "yes":
        water_another_plant = input("which plant would you like to water?")
        plant_watered.append(water_another_plant)
        print(plant_watered)
     elif which_plant_watered== "clear":
-        current_plants.clear()
+        plant_watered.clear()
 
-    with open(filename, 'wb') as file:
+    with open(saved_water_list, 'wb') as file:
         pickle.dump(plant_watered, file)
 
 
 
 print("Welcome to your plant notebook!")
 adding_plants_to_list()
-
+# adds additional plants to the list.
 add_more = input("would you like to add more plants to your list? \n").lower()
 if add_more == "yes":
     adding_plants_to_list()
 else:
     print("ok")
-
+#asks if you would like to water a plant. If yes it takes you to the watering_plants_function.
 water_main = input("would you like to water any plants?").lower()
 if water_main == "yes":
     watering_plants_func()
 else:
     print("ok")
-
+#prints current lists to che what is on them.
 print(f"your current plant list{current_plants}")
 print(f"current watered plants{plant_watered}")
 
